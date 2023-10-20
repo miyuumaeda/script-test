ff8ff25ba012c72df0d1d6d6bdfd6a313e35572b
miyuumaeda <146529928+miyuumaeda@users.noreply.github.com>
2023-10-20 18:10:44 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //コンソールに文字表示
+        Debug.Log("hello,world");
+    }
+
+    // Update is called once per frame
+
+}
+
+// Update はフレームごとに 1 回呼び出されます
+void Update()
+{
+
+}
