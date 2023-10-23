dc6c6bf55626a7d9dfc6880434b1a42d2472758d
miyuumaeda <146529928+miyuumaeda@users.noreply.github.com>
2023-10-23 10:53:43 +0900

    課題訂正

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,21 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //コンソールに文字表示
-        Debug.Log("hello,world");
+
+        // 要素数5の配列を初期化する
+        int[] arrey = new int[5];
+
+        // 5回処理を繰り返す
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(i);
+        }
+
+
+        // 逆順で5回処理を繰り返す
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(i);
+        }
+
     }
 
     // Update is called once per frame
-
-}
-
-// Update はフレームごとに 1 回呼び出されます
-void Update()
-{
-
+    void Update()
+    {
+        
+    }
 }
