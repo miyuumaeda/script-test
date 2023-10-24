e0bcd371381d912844c9ece346554b2d101fc3ef
miyuumaeda <146529928+miyuumaeda@users.noreply.github.com>
2023-10-24 20:12:33 +0900

    訂正

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -9,20 +9,21 @@ public class Test : MonoBehaviour
     {
 
         // 要素数5の配列を初期化する
-        int[] arrey = new int[5];
+        int[] array = new int[5];
 
-        // 5回処理を繰り返す
-        for (int i = 0; i < 5; i++)
-        {
-            Debug.Log(i);
-        }
+        array[0] = 10;
+        array[1] = 40;
+        array[2] = 20;
+        array[3] = 60;
+        array[4] = 50;
 
 
         // 逆順で5回処理を繰り返す
-        for (int i = 4; i >= 0; i--)
+        for (int i = array.Length - 1; i >= 0; i--)
         {
-            Debug.Log(i);
+            Debug.Log("array[" + i + "] = " + array[i]);
         }
+
 
     }
 
